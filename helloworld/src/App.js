e9c33edcdea24ca2242409d8cefa4ec846f7f328
Gerald Catina <geraldcatina5@gmail.com>
2025-06-11 09:00:50 +0800

    Commit Hello World

---
+++ helloworld/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="center">
+      <h1>Hello WOrld</h1>
+    </div>
+  );
+}
+
+export default App;
